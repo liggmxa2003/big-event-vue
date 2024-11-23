d16d4fd903dbd3cb17d73766f18f773ea7851e19
Ligg <156616301+liggmxa2003@users.noreply.github.com>
2024-11-24 07:24:50 +0800

    pinia持久化存储token

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from '@/router'
 import {createPinia} from 'pinia'
+import {createPersistedState} from 'pinia-persistedstate-plugin'
 
 import App from './App.vue'
 
 createApp(App)
 const app = createApp(App)
 const pinia = createPinia()
+const persist = createPersistedState()
+pinia.use(persist)
 app.use(pinia)
 app.use(router)
 app.use(ElementPlus);
