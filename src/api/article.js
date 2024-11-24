a4fc42d9a4ac0f130946d026a1e8e9d638304c2c
Ligg <156616301+liggmxa2003@users.noreply.github.com>
2024-11-24 22:09:33 +0800

    文章管理

--- src/api/article.js
+++ src/api/article.js
@@ -2,8 +2,6 @@ import request from '@/utils/request.js'
 import { useTokenStore } from '@/stores/token.js';
 //文章分类列表查询
 export const articleCategoryListService = ()=>{
-    //const tokenStore = useTokenStore();
-    //return request.get('/category',{headers:{'Authorization':tokenStore.token}});
     return request.get('/category');
 }
 
@@ -14,4 +12,8 @@ export const addArticleCategoryAddService = (categoryData)=>{
 //删除文章分类
 export const deleteArticleCategoryService = (id)=>{
     return request.delete('/category?id='+id);
+}
+//文章列表查询
+export const articleListService = (params)=>{
+    return request.get('/article',{params:params});
 }
